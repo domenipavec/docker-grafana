46f05203f5a6422599ddf74b89f90fc4c598b907
matematik7 <domen@ipavec.net>
2015-11-27 09:55:17 +0100

    Fix mysql configuration and add conf copy if it does not exist

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,10 @@ MAINTAINER Domen Ipavec <domen@ipavec.net>
 
 COPY init.sh /usr/bin/init.sh
 
+RUN cp -r /etc/grafana /etc/grafana-example
+
 EXPOSE 3000
 VOLUME ["/var/lib/grafana", "/var/log/grafana", "/etc/grafana"]
 
+ENTRYPOINT []
 CMD ["/usr/bin/init.sh"]
--- init.sh
+++ init.sh
@@ -2,7 +2,15 @@
 
 if [ ! -z ${MYSQL_PORT_3306_TCP_ADDR+x} ]; then
     export GF_DATABASE_TYPE=mysql
-    export GF_DATABASE_HOST=$MYSQL_PORT_3306_TCP_ADDR
+    export GF_DATABASE_HOST=$MYSQL_PORT_3306_TCP_ADDR:$MYSQL_PORT_3306_TCP_PORT
+fi
+
+if [ ! -f /etc/grafana/grafana.ini  ]; then
+    cp /etc/grafana-example/grafana.ini /etc/grafana/grafana.ini
+fi
+
+if [ ! -f /etc/grafana/ldap.toml  ]; then
+    cp /etc/grafana-example/ldap.toml /etc/grafana/ldap.toml
 fi
 
 /usr/sbin/grafana-server --homepath=/usr/share/grafana --config=/etc/grafana/grafana.ini cfg:default.paths.data=/var/lib/grafana cfg:default.paths.logs=/var/log/grafana
